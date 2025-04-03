6b96ee128fc5e6d2874449ae15067cb65918428b
ANISH-SR <rawatanish08@gmail.com>
2025-04-03 17:18:49 +0530

    day 2 - part2

--- ts/cluster1/spl_transfer.ts
+++ ts/cluster1/spl_transfer.ts
@@ -10,18 +10,44 @@ const commitment: Commitment = "confirmed";
 const connection = new Connection("https://api.devnet.solana.com", commitment);
 
 // Mint address
-const mint = new PublicKey("<mint address>");
+const mint = new PublicKey("9EmGYqp5od9RU62KgiWz2q3RxYfFuJu4AB2QVEZGSche");
 
 // Recipient address
-const to = new PublicKey("<receiver address>");
+const to = new PublicKey("Hhf7nz1BiwTLq9cp3cwjQ5BwwMy3hnWYthDDtL7UQqdF");
+
+const token_decimals = 1_000_000n;
 
 (async () => {
     try {
         // Get the token account of the fromWallet address, and if it does not exist, create it
+        const fromTokenAccount = await getOrCreateAssociatedTokenAccount(
+            connection,
+            keypair,
+            mint,
+            keypair.publicKey            
+        );
 
         // Get the token account of the toWallet address, and if it does not exist, create it
+        const toTokenAccount = await getOrCreateAssociatedTokenAccount(
+            connection,
+            keypair,
+            mint,
+            to
+        );
 
         // Transfer the new token to the "toTokenAccount" we just created
+        const signature = await transfer(
+            connection,
+            keypair,
+            fromTokenAccount.address,
+            toTokenAccount.address,
+            keypair,
+            token_decimals * 3n
+        );
+        //conform the transaction
+        console.log(`Sign of the transaction is ${signature}`);
+
+
     } catch(e) {
         console.error(`Oops, something went wrong: ${e}`)
     }
